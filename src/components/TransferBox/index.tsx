511e24881ce16381cb144102e47e055db31b6087
jarry-xiao <jarry.xiao@gmail.com>
2021-10-11 13:44:17 -0500

    Filter based off of form values

--- src/components/TransferBox/index.tsx
+++ src/components/TransferBox/index.tsx
@@ -1,17 +1,27 @@
-import React, { useEffect, useState } from "react";
+import React, { useMemo, useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
-import { cache, useConnection } from "../../contexts";
+import {
+  cache,
+  useConnection,
+  deserializeAccount,
+  deserializeMint,
+} from "../../contexts";
 import { FormControlUnstyled } from "@mui/material";
+import {
+  notify,
+  createAssociatedTokenAccountInstruction,
+  SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID,
+} from "../../utils";
+import { Schema, serialize } from "borsh";
+import { TOKEN_PROGRAM_ID, Token, NATIVE_MINT } from "@solana/spl-token";
 import { changeOffer, trade } from "../../actions/accept_offer";
 import { PublicKey } from "@solana/web3.js";
 import BN from "bn.js";
 import { useWallet } from "@solana/wallet-adapter-react";
 
-const displayMakerButton = () => {
-  
-}
+const displayMakerButton = () => {};
 
 export function TransferBox() {
   const connection = useConnection();
@@ -24,12 +34,94 @@ export function TransferBox() {
     maker: "",
   });
   const [accountState, setAccountState] = useState({});
+  const [mintCache, setMintCache] = useState({});
+  const [isSeller, setIsSeller] = useState(false);
 
-  useEffect(() => {}, [formState]);
+  useEffect(() => {
+    if (!wallet) {
+      return;
+    }
+    setIsSeller(wallet.publicKey?.toBase58() === formState.maker);
+  }, [formState, wallet, setIsSeller]);
 
-  useEffect(() => {}, [formState]);
-
-  useEffect(() => {}, [formState]);
+  useEffect(() => {
+    let subId;
+    const fetchAccountState = async () => {
+      let sellerWallet;
+      let sellerMint;
+      try {
+        sellerWallet = new PublicKey(formState.maker);
+        sellerMint = new PublicKey(formState.mintA);
+      } catch {
+        return;
+      }
+      const sellerTokenAccount = (
+        await PublicKey.findProgramAddress(
+          [
+            sellerWallet.toBuffer(),
+            TOKEN_PROGRAM_ID.toBuffer(),
+            sellerMint.toBuffer(),
+          ],
+          SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID
+        )
+      )[0];
+      const result = await connection.getAccountInfo(sellerTokenAccount);
+      if (result) {
+        console.log("Received account data");
+        const tokenAccount = deserializeAccount(result.data);
+        setAccountState(tokenAccount);
+      }
+      subId = connection.onAccountChange(sellerTokenAccount, (result) => {
+        if (result) {
+          console.log("Received account data");
+          try {
+            const tokenAccount = deserializeAccount(result.data);
+            setAccountState(tokenAccount);
+            console.log(accountState);
+          } catch (e) {
+            console.log("Failed to deserialize account", e);
+          }
+        }
+      });
+    };
+    const fetchMintState = async () => {
+      let mint;
+      for (const mintString of [formState.mintA, formState.mintB]) {
+        console.log("processing mint", mintString);
+        try {
+          mint = new PublicKey(mintString);
+        } catch(e) {
+          console.log("Invalid Pubkey");
+          console.log(formState)
+          continue;
+        }
+        if (!(mint.toBase58() in mintCache)) {
+          let result = await connection.getAccountInfo(mint);
+          if (result) {
+            try {
+              const mintData = deserializeMint(result.data)
+              console.log("MINT DATA", mintData);
+              setMintCache({
+                ...mintCache,
+                [mintString]: mintData,
+              });
+              console.log(mintCache);
+            } catch {
+              console.log("Invalid Mint");
+            }
+          }
+        } else {
+          console.log(mintCache[mintString]);
+        }
+      }
+    };
+    console.log(mintCache)
+    fetchAccountState();
+    fetchMintState();
+    return () => {
+      if (subId) connection.removeAccountChangeListener(subId);
+    };
+  }, [formState, wallet, setAccountState, setMintCache]);
 
   useEffect(() => {}, [formState]);
 
@@ -98,77 +190,83 @@ export function TransferBox() {
           }}
         />
       </div>
-      {}
       <div style={{ marginTop: "10px" }}>
-        <Button
-          variant="contained"
-          onClick={() => {
-            if (formState) {
-              try {
-                changeOffer(
-                  connection,
-                  new PublicKey(formState.mintA),
-                  new PublicKey(formState.mintB),
-                  new BN(parseInt(formState.sizeA)),
-                  new BN(parseInt(formState.sizeB)),
-                  wallet
-                );
-              } catch (e) {
-                return;
-              }
-            }
-          }}
-          sx={{ marginRight: "4px" }}
-        >
-          Open Offer
-        </Button>
-        <Button
-          variant="contained"
-          color="error"
-          sx={{ marginRight: "4px" }}
-          onClick={() => {
-            if (formState) {
-              try {
-                changeOffer(
-                  connection,
-                  new PublicKey(formState.mintA),
-                  new PublicKey(formState.mintB),
-                  new BN(parseInt(formState.sizeA)),
-                  new BN(parseInt(formState.sizeB)),
-                  wallet,
-                  false,
-                );
-              } catch (e) {
-                return;
-              }
-            }
-          }}
-        >
-          Close Offer
-        </Button>
-        <Button
-          variant="contained"
-          color="success"
-          onClick={() => {
-            if (formState) {
-              try {
-                trade(
-                  connection,
-                  new PublicKey(formState.maker),
-                  new PublicKey(formState.mintA),
-                  new PublicKey(formState.mintB),
-                  new BN(parseInt(formState.sizeA)),
-                  new BN(parseInt(formState.sizeB)),
-                  wallet
-                );
-              } catch (e) {
-                return;
-              }
-            }
-          }}
-        >
-          Trade
-        </Button>
+        {isSeller ? (
+          <div>
+            <Button
+              variant="contained"
+              onClick={() => {
+                if (formState) {
+                  try {
+                    changeOffer(
+                      connection,
+                      new PublicKey(formState.mintA),
+                      new PublicKey(formState.mintB),
+                      new BN(parseInt(formState.sizeA)),
+                      new BN(parseInt(formState.sizeB)),
+                      wallet
+                    );
+                  } catch (e) {
+                    return;
+                  }
+                }
+              }}
+              sx={{ marginRight: "4px" }}
+            >
+              Open Offer
+            </Button>
+            <Button
+              variant="contained"
+              color="error"
+              sx={{ marginRight: "4px" }}
+              onClick={() => {
+                if (formState) {
+                  try {
+                    changeOffer(
+                      connection,
+                      new PublicKey(formState.mintA),
+                      new PublicKey(formState.mintB),
+                      new BN(parseInt(formState.sizeA)),
+                      new BN(parseInt(formState.sizeB)),
+                      wallet,
+                      false
+                    );
+                  } catch (e) {
+                    return;
+                  }
+                }
+              }}
+            >
+              Close Offer
+            </Button>
+          </div>
+        ) : (
+          <div>
+            <Button
+              variant="contained"
+              color="success"
+              onClick={() => {
+                if (formState) {
+                  try {
+                    trade(
+                      connection,
+                      new PublicKey(formState.maker),
+                      new PublicKey(formState.mintA),
+                      new PublicKey(formState.mintB),
+                      new BN(parseInt(formState.sizeA)),
+                      new BN(parseInt(formState.sizeB)),
+                      wallet
+                    );
+                  } catch (e) {
+                    return;
+                  }
+                }
+              }}
+            >
+              Trade
+            </Button>
+          </div>
+        )}
       </div>
     </Box>
   );
